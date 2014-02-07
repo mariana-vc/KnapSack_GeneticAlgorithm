71895d592a2370d5dc2e4e924a8425241c921623
apreciad <apreciad@MS14K4.winad.msudenver.edu.>
2014-02-07 12:15:58 -0700

    -Initial creation of project

---
+++ GeneticKnapsack/src/geneticknapsack/GeneticKnapsack.java
@@ -0,0 +1,26 @@
+
+package geneticknapsack;
+
+/**
+ * This program will implement a genetic algorithm in the hopes of solving the
+ * knapsack problem through trial and error.  Values will be hardcoded for now
+ * but in the future may be dynamic.
+ * 
+ * Bio Computing
+ * Dr. Gurka
+ * February 7 2014
+ *
+ * @author Angel Preciado
+ */
+public class GeneticKnapsack {
+    
+    private int generations;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
