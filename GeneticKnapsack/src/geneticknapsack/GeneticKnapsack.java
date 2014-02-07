5aec7a3be569c8d1ce5e375a7ae8a96d70c33f57
apreciad <apreciad@MS14K4.winad.msudenver.edu.>
2014-02-07 16:34:53 -0700

    added methods for calculating average weights and values and framework added print methods for printing out populations and stats to the screen set up framework for running through iterations of the program

--- GeneticKnapsack/src/geneticknapsack/GeneticKnapsack.java
+++ GeneticKnapsack/src/geneticknapsack/GeneticKnapsack.java
@@ -1,6 +1,7 @@
 
 package geneticknapsack;
 
+import java.text.DecimalFormat;
 import java.util.Random;
 
 /**
@@ -16,10 +17,12 @@ import java.util.Random;
  */
 public class GeneticKnapsack {
     
-    private final int KNAPSACK_CAPACITY = 300;  //capacity of knapsack
-    private final int NUM_OF_ITEMS = 30;        //number of different items to consider
+    private final int KNAPSACK_CAPACITY = 100;  //capacity of knapsack
+    private final int NUM_OF_ITEMS = 10;        //number of different items to consider
     private final int MAX_GENERATIONS = 200;    //max generations before halting
-    private final int POPULATION_SIZE = 25;     //size of each population after generation
+    private final int POPULATION_SIZE = 10;   //size of each population after generation
+    private final int MAX_WEIGHT = 20;          //maximum weight allowed for an item, used for random item generation
+    private final int MAX_VALUE = 10;          //maximum value allowed for an item ^^^
     private int[][] population;
     private Item[] items;
     
@@ -29,10 +32,45 @@ public class GeneticKnapsack {
     }
     
     private void run() {
-        this.randomizeItems(15,30);
-        this.printItemArray(this.items);
+        this.randomizeItems(this.MAX_WEIGHT,this.MAX_VALUE);
+        this.createInitialPopulation();
+        int run = 0;
+        while(run < 3){
+            this.printPopulationAndStats(run);
+            run++;
+        }
+        //this.print2dArray(population);
+        //this.printItemArray(items);
+        //System.out.println(this.getPopAverageWeight());
+        //System.out.println(this.getPopAverageValue());
     }
 
+    /**
+     * Method for printing out data to the console.
+     * @param gen generation number
+     */
+    public void printPopulationAndStats(int gen){
+        System.out.println("Population for Generation "+gen+": \n");
+        this.print2dArray(population);
+        System.out.println("\nAverage Population Weight: "+this.getPopAverageWeight());
+        System.out.println("Average Population Value: "+this.getPopAverageValue()+"\n");
+        System.out.println("---------------------------------------------------------");
+    }
+    
+    /**
+     * This method will randomly assign 1's and 0's to an individuals sack
+     * 1 means it contains an item, 0 means it does not, this will keep track
+     * of which items are currently in this individuals knapsack
+     */
+    public void createInitialPopulation(){
+        Random rand = new Random();
+        for (int i = 0; i < this.POPULATION_SIZE; i++) {
+            for (int j = 0; j < this.NUM_OF_ITEMS; j++) {
+                population[i][j] = rand.nextInt(2);
+            }
+        }
+    }
+    
     /**
      * This method will initialize an array that will contain objects along with their
      * weights and values.
@@ -57,13 +95,100 @@ public class GeneticKnapsack {
             this.value = v;
         }
     }
+    /**
+     * Returns the average weight of all individuals, but only if they are at or
+     * below the capacity.
+     * @return double containing average
+     */
+    public double getPopAverageWeight(){
+        DecimalFormat df = new DecimalFormat("#.##");
+        int count = 0;
+        int weight = 0;
+        for (int i = 0; i < this.POPULATION_SIZE; i++) {
+            if(this.getIndividualWeight(i) <= this.KNAPSACK_CAPACITY){
+            weight += this.getIndividualWeight(i);
+            count++;
+            }
+        }
+        if(count != 0){
+            double avg = (((double)weight) / ((double)count));
+            df.format(avg);
+            return avg;
+        } else {
+            return -1;
+        }
+    }
     
     /**
-     * just for show
+     * Will get the average value for all individuals if they are under the weight
+     * capacity.
+     * @return average value
+     */
+    public double getPopAverageValue(){
+        DecimalFormat df = new DecimalFormat("#.##");
+        int count = 0;
+        int value = 0;
+        for (int i = 0; i < this.POPULATION_SIZE; i++) {
+            if(this.getIndividualWeight(i) <= this.KNAPSACK_CAPACITY){
+            value += this.getIndividualValue(i);
+            count++;
+            }
+        }
+        if(count != 0){
+        double avg = ((double)value / (double)count);
+            df.format(avg);
+            return avg;
+        } else {
+            return -1;
+        }
+    }
+    
+    /**
+     * Will return the total weight of an individual at index i in the population array
+     * @param i index of individual
+     * @return int total weight of items in sack
+     */
+    public int getIndividualWeight(int i){
+        int weight = 0;
+        for (int j = 0; j < this.NUM_OF_ITEMS; j++) {
+            if(this.population[i][j] == 1){
+                weight += items[j].weight;
+            }
+        }
+        return weight;
+    }
+    
+    public int getIndividualValue(int i){
+        int value = 0;
+        for (int j = 0; j < this.NUM_OF_ITEMS; j++) {
+            if(this.population[i][j] == 1){
+                value += items[j].value;
+            }
+        }
+        return value;
+    }
+    
+    /**
+     * just for showing me whats inside
      */
     public void printItemArray(Item[] items){
         for (int i = 0; i < items.length; i++) {
             System.out.println(items[i].weight + " : "+items[i].value);
+        }
+    }
+    
+    /**
+     * shows me whats inside.
+     * @param pop 
+     */
+    public void print2dArray(int[][] pop){
+        int counter = 0;
+        for (int i = 0; i < this.POPULATION_SIZE; i++) {
+            for (int j = 0; j < this.NUM_OF_ITEMS; j++) {
+                System.out.print(pop[i][j]);
+                counter++;
+            }
+            System.out.println();
         }
     }
     
